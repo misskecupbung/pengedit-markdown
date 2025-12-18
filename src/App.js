278ed255b99a63d5c4a93e6e373e17a55092cd76
Ananda Dwi Rahmawati <anandadwirahmawati@acpcomputer.edu.sg>
2025-12-18 16:41:40 +0800

    chore: remove featured projects

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import { useState } from "react";
 import Yamde from "yamde";
-
 import { ContentExample } from "./ContentExample";
+import "./App.css";
 
 export default function App() {
   const [isLightMode, setIsLightMode] = useState(true);
   const [text, setText] = useState(ContentExample);
 
   return (
-    <div className="App">
-      <div>
-        <h1><center>Pengedit Markdown</center></h1>
-      </div>
-      <div>
-        <h4><center>Created by: Ananda</center></h4>
-      </div>
-    <div className="btn btn-primary" onClick={() => setIsLightMode(!isLightMode)}>
-      {`${isLightMode ? "Dark" : "Light"} Mode`}
-    </div>
+    <div className={`App ${isLightMode ? "light" : "dark"}`}>
+      <header className="App-header">
+        <h1>Pengedit Markdown</h1>
+        <h4>Created by: Ananda</h4>
+        <button
+          className="mode-toggle btn btn-primary"
+          onClick={() => setIsLightMode(!isLightMode)}
+        >
+          {isLightMode ? "Dark Mode" : "Light Mode"}
+        </button>
+      </header>
 
-      <Yamde
-        value={text}
-        handler={setText}
-        theme={isLightMode ? "light" : "dark"}
-      />
+      <main>
+        <Yamde
+          value={text}
+          handler={setText}
+          theme={isLightMode ? "light" : "dark"}
+        />
+      </main>
     </div>
   );
 }
